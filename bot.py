d053703bc1d662a04c3235db759b136b6b82ef36
Evgenij Karpov <gedrip87@gmail.com>
2021-08-21 02:13:48 +0300

    Now our bot can recognize cats and save them as well

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import logging
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
+from anketa import (anketa_start, anketa_name, anketa_rating, anketa_skip, anketa_comment,
+                    anketa_dontknow)
 from handlers import (greet_user, guess_number, send_cat_picture,
-                         user_coordinates, talk_to_me)
+                         user_coordinates, talk_to_me, check_user_photo)
 import settings
 
-logging.basicConfig(filename='bot4.log', level=logging.INFO)
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
@@ -15,10 +17,29 @@ def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
+
+    anketa = ConversationHandler(
+        entry_points=[
+            MessageHandler(Filters.regex('^(Заполнить анкету)$'), anketa_start)
+        ],
+        states={
+            "name": [MessageHandler(Filters.text, anketa_name)],
+            "rating": [MessageHandler(Filters.regex("^(1|2|3|4|5)$"), anketa_rating)],
+            "comment": [
+                CommandHandler("skip", anketa_skip),
+                MessageHandler(Filters.text, anketa_comment)
+                ]
+        },
+        fallbacks=[
+            MessageHandler(Filters.text| Filters.photo | Filters.video| Filters.document| Filters.location, anketa_dontknow)
+        ]
+    )
+    dp.add_handler(anketa)
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(MessageHandler(Filters.regex('^(Прислать котика)$'), send_cat_picture))
+    dp.add_handler(MessageHandler(Filters.photo, check_user_photo))
     dp.add_handler(MessageHandler(Filters.location, user_coordinates))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
+
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import service_pb2_grpc
+from clarifai_grpc.grpc.api import service_pb2, resources_pb2
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
+
+
+
+#from clarifai.rest import ClarifaiApp
 from emoji import emojize
+#from pprint import PrettyPrinter
 from random import randint, choice
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 
@@ -21,4 +32,52 @@ def play_random_numbers(user_number):
     return message
 
 def main_keyboard():
-    return ReplyKeyboardMarkup([['Прислать котика', KeyboardButton('Мои координаты', request_location=True)]])
+    return ReplyKeyboardMarkup([
+        ['Прислать котика', KeyboardButton('Мои координаты', request_location=True), 'Заполнить анкету']
+    ])
+
+
+# NEW VERSION
+def is_cat2(url):
+    stub = service_pb2_grpc.V2Stub(ClarifaiChannel.get_grpc_channel())
+    # This is how you authenticate.
+    metadata = (('authorization', f'Key {settings.CLARIFAI_API_KEY}'),)
+
+    request = service_pb2.PostModelOutputsRequest(
+    # This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id='aaa03c23b3724a16a56b629203edc62c',
+        inputs=[
+        resources_pb2.Input(data=resources_pb2.Data(image=resources_pb2.Image(url=url)))
+        ])
+    response = stub.PostModelOutputs(request, metadata=metadata)
+
+    if response.status.code != status_code_pb2.SUCCESS:
+        raise Exception("Request failed, status code: " + str(response.status.code))
+
+    for concept in response.outputs[0].data.concepts:
+        print('%12s: %.2f' % (concept.name, concept.value)) # Для проверки паттерна в консоле
+        if concept.name == 'cat' and concept.value >= 0.95:
+            return True
+    else:
+        return False
+
+# OLD
+"""
+def is_cat(file_name):
+    app = ClarifaiApp(api_key=settings.CLARIFAI_API_KEY)
+    model = app.public_models.general_model
+    response = model.predict_by_filename(file_name, max_concepts=5)
+    if response['status']['code'] == 10000:
+        for concept in response['outputs'][0]['data']['concepts']:
+            if concept['name'] == 'cat':
+                return True
+    return False
+
+"""
+
+#if __name__ == "__main__":
+    #pp = PrettyPrinter(indent=2)
+    #pp.pprint(is_cat("images/cat1.jpeg"))
+    #pp.pprint(is_cat("images/not_cat.jpg"))
+    #print(is_cat("images/cat1.jpg"))
+    #print(is_cat("images/not_cat.jpg"))
